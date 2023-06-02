acd676e27c04b0ddb15bd00fd0493013b5095dea
GabrielPinheiroMatiucci <gabriel_1505@outlook.com>
2023-06-01 21:16:06 -0300

    feat: add solution to challenge 1.

---
+++ challenge1.py
@@ -0,0 +1,2 @@
+def solutionChallengeOne(list1: list, list2: list):
+    return list(set(list1).intersection(list2))
---
+++ index.py
@@ -0,0 +1,14 @@
+from challenge1 import solutionChallengeOne
+
+print('Desafio 1')
+print('list1: [1, 2, 3]  list2: [2, 4]')
+print(solutionChallengeOne([1, 2, 3], [2, 4]))
+print('list1: [1, 2, 3]  list2: [1, 2, 3, 3, 4]')
+print(solutionChallengeOne([1, 2, 3], [1, 2, 3, 3, 4]))
+print('')
+
+print('Desafio 2')
+print('')
+
+print('Desafio 3')
+print('')
